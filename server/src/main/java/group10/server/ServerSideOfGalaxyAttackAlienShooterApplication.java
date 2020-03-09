2963daa9c7bd77e7bb4445866d75e19dd242eb30
bayra <berkantbayraktar06@gmail.com>
2020-03-09 15:13:22 +0300

    Initialize maven project structure for Galaxy Attack Alien Shooter app.

---
+++ server/src/main/java/group10/server/ServerSideOfGalaxyAttackAlienShooterApplication.java
@@ -0,0 +1,13 @@
+package group10.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideOfGalaxyAttackAlienShooterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSideOfGalaxyAttackAlienShooterApplication.class, args);
+    }
+
+}
