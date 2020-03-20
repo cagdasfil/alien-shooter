d4e3394f1952eb1fabd55db21eac09b727c004cf
berkant.bayraktar <berkantbayraktar06@gmail.com>
2020-03-20 22:25:36 +0300

    Implement user class.

---
+++ server/src/main/java/group10/server/User/User.java
@@ -0,0 +1,87 @@
+package group10.server.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Integer id;
+
+    @Column(name ="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="surname")
+    private String surname;
+
+    @Column(name="email")
+    private String email;
+
+
+    public User(Integer id, String username, String password, String name, String surname, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
