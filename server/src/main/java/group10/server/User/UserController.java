8e09b4e2555b0addea0ce6b79b4fda1c22f095bc
berkant.bayraktar <e2098796@ceng.metu.edu.tr>
2020-03-21 18:15:27 +0300

    Implement controller and service layers for User class. For now, we can deal with GET, POST, UPDATE and DELETE requests.

---
+++ server/src/main/java/group10/server/User/UserController.java
@@ -0,0 +1,43 @@
+package group10.server.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return userService.getAllUsers();
+    }
+
+
+    @GetMapping("/users/{id}")
+    public User getUsersById(@PathVariable(value = "id") Long userId) {
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/users/{id}")
+    public void updateUser(@RequestBody User userDetails, @PathVariable(value = "id") Long userId) {
+        userService.updateUser(userId,userDetails);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@Valid @RequestBody User user) {
+        userService.addUser(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable(value = "id") Long userId){
+        userService.deleteUser(userId);
+    }
+
+
+}
---
+++ server/src/main/java/group10/server/User/UserService.java
@@ -0,0 +1,43 @@
+package group10.server.User;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public void addUser(User user){
+        userRepository.save(user);
+    }
+
+    public User getUser(Long id ){
+       Optional<User> users = userRepository.findById(id);
+       return users.get();
+
+    }
+
+    public void updateUser(Long userId, User userDetails) {
+        User u = getUser(userId);  // find the user that will be modified by id's.
+        u.setEmail(userDetails.getEmail());
+        u.setName(userDetails.getName());
+        u.setSurname(userDetails.getSurname());
+        u.setPassword(userDetails.getPassword());
+        userRepository.save(u);
+    }
+
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
