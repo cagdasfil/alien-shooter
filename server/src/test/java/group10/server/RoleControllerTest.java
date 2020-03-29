27e24d936a81e715718aa9ca26bcab57d0f79ab2
cagdas.fil <e2093839@ceng.metu.edu.tr>
2020-03-29 13:24:36 +0300

    Exception test added to RoleControllerTest

--- server/src/test/java/group10/server/RoleControllerTest.java
+++ server/src/test/java/group10/server/RoleControllerTest.java
@@ -19,12 +19,13 @@ public class RoleControllerTest extends ServerTest{
 
     @Test
     public void creatingRole() throws Exception{
-        MockHttpServletRequestBuilder postUserRequest = MockMvcRequestBuilders.post("/roles")
+
+        MockHttpServletRequestBuilder postRoleRequest = MockMvcRequestBuilders.post("/roles")
                 .content("{\"roleId\":3," +
                         "\"role\":\"TRY\"}")
                 .contentType(MediaType.APPLICATION_JSON);
 
-        mockMvc.perform(postUserRequest).andExpect(status().isOk());
+        mockMvc.perform(postRoleRequest).andExpect(status().isOk());
 
         Optional<Role> role = roleRepository.findById(3L);
         assertEquals(role.isPresent(), true);
@@ -33,7 +34,22 @@ public class RoleControllerTest extends ServerTest{
 
     @Test
     public void gettingRoles() throws Exception{
+
         mockMvc.perform(MockMvcRequestBuilders.get("/roles")).andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
     }
+
+    @Test
+    public void testBadRequestException() throws Exception{
+
+        MockHttpServletRequestBuilder postRoleRequest = MockMvcRequestBuilders.post("/roles")
+                .content("{\"roleId\":\"random_string\"," +
+                        "\"role\":\"TRY\"}")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(postRoleRequest).andExpect(status().isBadRequest());
+
+    }
+
 }
