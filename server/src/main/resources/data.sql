15488cf123207876e1ef27181e200ccfa13e1077
berkant.bayraktar <e2098796@ceng.metu.edu.tr>
2020-03-29 13:04:39 +0300

    modify data.sql

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into role(role) values ('USER');
 insert into role(role) values ('ADMIN');
-insert into user(username,password,name,surname,email) values ('user','user','user','user','user@gmail.com');
-insert into user(username,password,name,surname,email) values ('admin','admin','admin','admin','admin@gmail.com');
+insert into role(role) values ('USER');
+insert into user(username,password,name,surname,email,active) values ('admin','admin','admin','admin','admin@gmail.com',1);
+insert into user(username,password,name,surname,email,active) values ('user','user','user','user','user@gmail.com',1);
 insert into user_role(user_id,role_id) values (1,1);
-insert into user_role() values (2,2);
+insert into user_role(user_id,role_id) values (2,2);
