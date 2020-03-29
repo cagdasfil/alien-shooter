a9818ceea26f421d348567270296ad8ec985fb48
berkant.bayraktar <e2098796@ceng.metu.edu.tr>
2020-03-29 21:51:34 +0300

    add postman collection to repository.

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into role(role) values ('ADMIN');
 insert into role(role) values ('USER');
-insert into user(username,password,name,surname,email,active) values ('admin','admin','admin','admin','admin@gmail.com',1);
-insert into user(username,password,name,surname,email,active) values ('user','user','user','user','user@gmail.com',1);
+insert into user(username,password,name,surname,email,active) values ('admin','$2a$05$Xm9ODm79wA9RQEBT5Vo5S.Q1u/8QMYi.I8.xvs59uQJ6WhJ5zrbSO','admin','admin','admin@gmail.com',1);
+insert into user(username,password,name,surname,email,active) values ('user','$2a$05$gTFIylLkjPwzKNIO6i2RCuUNFfc4Wcpr.CxZ6ZInibAIW8gi6bqCS','user','user','user@gmail.com',1);
 insert into user_role(user_id,role_id) values (1,1);
 insert into user_role(user_id,role_id) values (2,2);
