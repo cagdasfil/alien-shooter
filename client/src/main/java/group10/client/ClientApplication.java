5f94fd52743f806fd59e6f66c89abc04f484fc1b
berkant.bayraktar <e2098796@ceng.metu.edu.tr>
2020-04-21 23:16:47 +0300

    Revert "Implement entrance screen for the game."
    This reverts commit c7ffb0f4ccfa651f52f0cf4f1f5d3f4aebae7e29.

--- client/src/main/java/group10/client/ClientApplication.java
+++ client/src/main/java/group10/client/ClientApplication.java
@@ -1,55 +1,14 @@
 package group10.client;
 
-import group10.client.game.Game;
-import group10.client.view.MainScreen;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
-
-public class ClientApplication extends Application {
-
-    // API URL
-    public static String hostName = "144.122.71.144:8080/";
-
-    // Global scene for stage
-    public static Scene mainScene;
-
-    //  loggedPlayer score
-    public static Integer score = 0;
-
-    // Screen width and height
-    private int screenWidth = 800;
-    private int screenHeight = 900;
-    private Game game = new Game();
-
-
-    @Override
-    public void start(Stage primaryStage){
-
-
-        // First scene of the application
-        MainScreen startScreen = new MainScreen("Alien Shooter App","Play");
-        startScreen.linkPlayButton(game);
-        mainScene = new Scene(startScreen,screenWidth,screenHeight);
-
-
-
-        // Set title
-        primaryStage.setTitle("ALIEN SHOOTER");
-
-        // Forbid the resizing of the screen
-        primaryStage.setResizable(false);
-        primaryStage.setScene(mainScene);
-        primaryStage.setResizable(true);
-        primaryStage.show();
-    }
-
+@SpringBootApplication
+public class ClientApplication {
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(UiApplication.class, args);
     }
-}
 
+}
