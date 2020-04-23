5a66e9ee15ca29ef4cd41ecc556a0c7116b80a4d
cagdas.fil <e2093839@ceng.metu.edu.tr>
2020-04-23 14:11:17 +0300

    sign up page designed.

--- client/src/main/java/group10/client/controller/SignUpController.java
+++ client/src/main/java/group10/client/controller/SignUpController.java
@@ -1,15 +1,25 @@
 package group10.client.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.AnchorPane;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 @Component
 public class SignUpController implements Initializable {
+
+    @FXML public AnchorPane generalLayout;
 
     private RestTemplate restTemplate;
     @Value("${spring.application.apiAddress}") private String apiAddress;
@@ -17,5 +27,11 @@ public class SignUpController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    @FXML
+    public void backClick() throws IOException {
+        Parent login = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("Login.fxml")));
+        generalLayout.getChildren().setAll(login);
     }
 }
