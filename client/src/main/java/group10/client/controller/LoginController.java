d79c582fcaeb14d2024c45d277d05b122b7f6413
cagdas.fil <e2093839@ceng.metu.edu.tr>
2020-04-25 16:56:16 +0300

    saving score for new user bug fixed.

--- client/src/main/java/group10/client/controller/LoginController.java
+++ client/src/main/java/group10/client/controller/LoginController.java
@@ -33,7 +33,9 @@ public class LoginController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        apiAddress = applicationApiAddress;
+        if(applicationApiAddress != null){
+            apiAddress = applicationApiAddress;
+        }
     }
 
     @FXML
