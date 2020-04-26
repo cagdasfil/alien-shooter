f62ef506d56b802306fbf3767eb5a3a903c0ac13
cagdas.fil <e2093839@ceng.metu.edu.tr>
2020-04-26 19:24:36 +0300

    JavaDoc created for ScoreApi and UserApi

--- client/src/main/java/group10/client/api/ScoreApi.java
+++ client/src/main/java/group10/client/api/ScoreApi.java
@@ -8,23 +8,42 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * This class runs score related HTTP methods.
+ */
 public class ScoreApi {
 
     private static final RestTemplate restTemplate = new RestTemplate();
     private static final String apiAddress = LoginController.apiAddress;
 
+    /**
+     * This method returns all scores created in last week.
+     * @return weekly scores as JSON string.
+     */
     public static String getScoresWeekly(){
         return restTemplate.getForObject(apiAddress + "/scores/weekly" , String.class);
     }
 
+    /**
+     * This method returns all scores created in last month.
+     * @return monthly scores as JSON string.
+     */
     public static String getScoresMonthly(){
         return restTemplate.getForObject(apiAddress + "/scores/monthly" , String.class);
     }
 
+    /**
+     * This method returns all scores.
+     * @return all scores as JSON string.
+     */
     public static String getScoresAllTime(){
         return restTemplate.getForObject(apiAddress + "/scores/alltime" , String.class);
     }
 
+    /**
+     * This method saves score of the logged in user.
+     * @param score score of the user.
+     */
     public static void saveScore(Integer score){
             String jsonString = new JSONObject()
                     .put("score", score).toString();
--- client/src/main/java/group10/client/api/UserApi.java
+++ client/src/main/java/group10/client/api/UserApi.java
@@ -8,11 +8,23 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * This class runs user related HTTP methods.
+ */
 public class UserApi {
 
     private static final RestTemplate restTemplate = new RestTemplate();
     private static final String apiAddress = LoginController.apiAddress;
 
+    /**
+     * This method signs up a user with given user information.
+     * @param username username of the user
+     * @param password password of the user
+     * @param name name of the user
+     * @param surname surname of the user
+     * @param email e-mail of the user
+     * @return response of the HTTP method as JSONObject
+     */
     public static ResponseEntity<JSONObject> signUpUser(String username, String password, String name, String surname, String email){
 
         String jsonString = new JSONObject()
@@ -34,6 +46,12 @@ public class UserApi {
 
     }
 
+    /**
+     * This method authenticates the user with given information.
+     * @param username username of the user
+     * @param password password of the user
+     * @return response of the HTTP method as String
+     */
     public static String loginUser(String username, String password){
 
         HttpHeaders headers = new HttpHeaders();
@@ -51,6 +69,11 @@ public class UserApi {
                 String.class).toString();
     }
 
+    /**
+     * This method gets User object of the given username
+     * @param username username of the user
+     * @return user as a User object
+     */
     public static User getUser(String username){
         return restTemplate.getForObject(apiAddress + "/usernames/"+username , User.class);
     }
