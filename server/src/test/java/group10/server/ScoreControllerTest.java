04c121c2933c948371a3dbf41d06df94d9fa64ff
cagdas.fil <e2093839@ceng.metu.edu.tr>
2020-04-26 16:20:32 +0300

    unit test of ScoreController improved

--- server/src/test/java/group10/server/ScoreControllerTest.java
+++ server/src/test/java/group10/server/ScoreControllerTest.java
@@ -1,10 +1,16 @@
 package group10.server;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.gson.Gson;
 import group10.server.model.Score;
 import group10.server.model.User;
+import net.minidev.json.JSONValue;
 import org.apache.commons.lang.StringUtils;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -12,12 +18,15 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.validation.constraints.AssertFalse;
 import java.lang.reflect.Array;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,7 +34,98 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ScoreControllerTest extends ServerTest{
 
     @Test
-    public void addNewScoreThenGet() throws Exception{
+    public void getAllScores() throws Exception{
+
+        /* Run get /scores method to get all scores */
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/scores")).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        /* Get list of scores as String */
+        String scoresString = mvcResult.getResponse().getContentAsString();
+
+        /* Check whether scores exist */
+        assertFalse(scoresString.isEmpty());
+
+    }
+
+    @Test
+    public void getAllScoresWeekly() throws Exception{
+
+        /* Run get /scores/weekly method to get all scores */
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/scores/weekly")).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        /* Get list of scores as String */
+        String scoresString = mvcResult.getResponse().getContentAsString();
+
+        /* Find dates in returned scores */
+        Pattern pattern = Pattern.compile("createdAt\":\"(.*?)\"", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(scoresString);
+
+        /* Check whether scores are created in last week */
+        while (matcher.find()) {
+            LocalDate localDate = LocalDate.parse(matcher.group(1));
+            assertTrue(localDate.isAfter(LocalDate.now().minusDays(8)));
+        }
+
+    }
+
+    @Test
+    public void getAllScoresMonthly() throws Exception{
+
+        /* Run get /scores/monthly method to get all scores */
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/scores/monthly")).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        /* Get list of scores as String */
+        String scoresString = mvcResult.getResponse().getContentAsString();
+
+        /* Find dates in returned scores */
+        Pattern pattern = Pattern.compile("createdAt\":\"(.*?)\"", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(scoresString);
+
+        /* Check whether scores are created in last month */
+        while (matcher.find()) {
+            LocalDate localDate = LocalDate.parse(matcher.group(1));
+            assertTrue(localDate.isAfter(LocalDate.now().minusDays(31)));
+        }
+
+    }
+
+    @Test
+    public void getAllScoresAllTime() throws Exception{
+
+        /* Run get /scores/alltime method to get all scores */
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/scores/alltime")).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        /* Get list of scores as String */
+        String scoresString = mvcResult.getResponse().getContentAsString();
+
+        /* Find scores in returned scores */
+        Pattern pattern = Pattern.compile("score\":(.*?),", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(scoresString);
+
+        /* Create score variables */
+        long highScore = 0L;
+        long lowScore = 0L;
+
+        /* Take first returned score */
+        if (matcher.find()){
+            highScore = Long.parseLong(matcher.group(1));
+        }
+
+        /* Check whether scores are returned in order */
+        while (matcher.find()) {
+            lowScore = Long.parseLong(matcher.group(1));
+            assertTrue( highScore >= lowScore );
+            highScore = lowScore;
+        }
+
+    }
+
+    @Test
+    public void addNewScoreThenGetScoreThenDeleteUser() throws Exception{
 
         /* Create post request for new user */
         MockHttpServletRequestBuilder postUserRequest = MockMvcRequestBuilders.post("/sign_up")
@@ -56,46 +156,29 @@ public class ScoreControllerTest extends ServerTest{
         MockHttpServletRequestBuilder postScoreRequest = MockMvcRequestBuilders.post("/scores/"+testUser.getId().toString())
                 .content("{\"user\":"+ userString +"," +
                         "\"score\":100," +
-                        "\"createdAt\":\"2020-03-28\"}")
+                        "\"createdAt\":\"2020-03-21\"}")
                 .contentType(MediaType.APPLICATION_JSON);
 
         /* Send request to the server */
         mockMvc.perform(postScoreRequest).andExpect(status().isOk());
 
-        /* Delete the user */
+        /* Delete the user. Deleting user cascades to delete score */
         mockMvc.perform(MockMvcRequestBuilders.delete("/users/"+testUser.getId().toString())).andExpect(status().isOk());
 
-    }
-
-    @Test
-    public void getAllScores() throws Exception{
-
         /* Run get /scores method to get all scores */
-        mockMvc.perform(MockMvcRequestBuilders.get("/scores")).andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/scores")).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        /* Get list of scores as String */
+        String scoresString = mvcResult.getResponse().getContentAsString();
+
+        /* Check whether there are no scores for specified user */
+        assertFalse(scoresString.contains("\"username\":\"testusername4\""));
 
     }
 
     @Test
-    public void getAllScoresWeekly() throws Exception{
-
-        /* Run get /scores/weekly method to get all scores created in last week */
-        mockMvc.perform(MockMvcRequestBuilders.get("/scores/weekly")).andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-
-    }
-
-    @Test
-    public void getAllScoresMonthly() throws Exception{
-
-        /* Run get /scores/monthly method to get all scores created in last month */
-        mockMvc.perform(MockMvcRequestBuilders.get("/scores/monthly")).andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-
-    }
-
-    @Test
-    public void addNewScoreThenDelete() throws Exception{
+    public void addNewScoreThenDeleteScore() throws Exception{
 
         /* Create post request for new user */
         MockHttpServletRequestBuilder postUserRequest = MockMvcRequestBuilders.post("/sign_up")
@@ -137,13 +220,29 @@ public class ScoreControllerTest extends ServerTest{
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andReturn();
 
         /* Convert returned user from json to string */
-        String scores = resultScore.getResponse().getContentAsString();
+        String scoresString = resultScore.getResponse().getContentAsString();
 
-        /* Find the scoreId in returned scores */
-        String scoreId = StringUtils.substringBetween(scores, "scoreId\":", ",");
+        /* Find the scoreId of the user in returned*/
+        Pattern pattern = Pattern.compile("scoreId\":(.?),\"user\":\\x7b\"id\":\\d+,\"username\":\"testusername5", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(scoresString);
+        String scoreId = "1";
+        if (matcher.find()){
+            scoreId = matcher.group(1);
+        }
 
         /* Delete the score */
         mockMvc.perform(MockMvcRequestBuilders.delete("/scores/"+scoreId)).andExpect(status().isOk());
+
+        /* Get scores with get /scores method again */
+        resultScore = mockMvc.perform(MockMvcRequestBuilders.get("/scores")).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        /* Convert returned scores from json to string */
+        scoresString = resultScore.getResponse().getContentAsString();
+
+        /* Try to find the scoreId of the deleted score*/
+        matcher = pattern.matcher(scoresString);
+        assertFalse(matcher.find());
 
         /* Delete the user */
         mockMvc.perform(MockMvcRequestBuilders.delete("/users/"+testUser.getId().toString())).andExpect(status().isOk());
