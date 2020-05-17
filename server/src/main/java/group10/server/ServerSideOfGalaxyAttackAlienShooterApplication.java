b276be1af2604271eed0caf9da74fa72b49abc7a
cagdas.fil <e2093839@ceng.metu.edu.tr>
2020-05-17 12:24:05 +0300

    pom.xml for server configured to create .war file

--- server/src/main/java/group10/server/ServerSideOfGalaxyAttackAlienShooterApplication.java
+++ server/src/main/java/group10/server/ServerSideOfGalaxyAttackAlienShooterApplication.java
@@ -2,9 +2,10 @@ package group10.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServerSideOfGalaxyAttackAlienShooterApplication {
+public class ServerSideOfGalaxyAttackAlienShooterApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerSideOfGalaxyAttackAlienShooterApplication.class, args);
