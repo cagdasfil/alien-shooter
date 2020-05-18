0f02c4675e6b85c39a7bbed8524ae030983c0d24
berkant.bayraktar <e2098796@ceng.metu.edu.tr>
2020-05-18 22:22:33 +0300

    Implement Socket client to be able to communicate between multiplayer pairs

---
+++ client/src/main/java/group10/client/multiplayer/SocketClient.java
@@ -0,0 +1,52 @@
+package group10.client.multiplayer;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class SocketClient {
+    private InetAddress host;
+    private Socket socket;
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+
+    public SocketClient() throws IOException, ClassNotFoundException, InterruptedException {
+
+        initConnections();
+    }
+
+    public void initConnections() throws IOException {
+        //get the localhost IP address, if server is running on some other IP, you need to use that
+        this.host = InetAddress.getLocalHost();
+        //establish socket connection to server
+        this.socket = new Socket(host.getHostName(), 9876);
+        //write to socket using ObjectOutputStream
+        this.oos = new ObjectOutputStream(socket.getOutputStream());
+        //read the server response message
+        this.ois = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void closeConnections() throws IOException {
+        //close resources
+        ois.close();
+        oos.close();
+        System.out.println("client is closed");
+    }
+
+    public void sendMessage(String message) throws IOException {
+        oos.writeObject(message);
+        System.out.println("Sending request to Socket Server" + message);
+        if(message.equals("exit")){
+            closeConnections();
+        }
+    }
+
+    public String readMessage() throws IOException, ClassNotFoundException, InterruptedException {
+        String message = (String) ois.readObject();
+        System.out.println("Message: " + message);
+        Thread.sleep(10);
+        return message;
+    }
+}
