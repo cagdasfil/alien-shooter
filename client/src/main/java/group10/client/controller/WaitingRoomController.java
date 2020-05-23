408b76d66122b0fc1daa6fad3720bd3e518ab8fc
cagdas.fil <e2093839@ceng.metu.edu.tr>
2020-05-23 18:46:35 +0300

    Controller implemented for WaitingRoom.

--- client/src/main/java/group10/client/controller/WaitingRoomController.java
+++ client/src/main/java/group10/client/controller/WaitingRoomController.java
@@ -1,7 +1,11 @@
 package group10.client.controller;
 
+import group10.client.api.MatchApi;
+import group10.client.api.UserApi;
 import group10.client.game.Game;
+import group10.client.model.server.Match;
 import group10.client.multiplayer.MultiplayerGame;
+import group10.client.multiplayer.SocketClient;
 import group10.client.multiplayer.SocketServer;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +14,8 @@ import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.Pane;
+import org.json.JSONObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -37,16 +43,43 @@ public class WaitingRoomController implements Initializable {
     }
 
     @FXML
-    public void readyClick() throws IOException {
-        SocketServer socketServer = null;
-        try {
-            socketServer = new SocketServer();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+    public void readyClick() throws IOException, InterruptedException {
+
+        Match match = MatchApi.getMatch();
+
+        if(match == null){ // Server
+            ResponseEntity<JSONObject> response = MatchApi.addMatch(
+                    LoginController.user.getUsername()
+            );
+            do {
+                match = MatchApi.getMatch();
+            } while (match.getClient_player().equals(""));
+            SocketServer socketServer = null;
+            try {
+                socketServer = new SocketServer();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            MultiplayerGame game = new MultiplayerGame(5, socketServer);
+            game.setFocusTraversable(true);
+            generalLayout.getChildren().setAll(game);
         }
-        MultiplayerGame game = new MultiplayerGame(5, socketServer);
-        game.setFocusTraversable(true);
-        generalLayout.getChildren().setAll(game);
+        else{ // Client
+            match.setClient_player(LoginController.user.getUsername());
+            MatchApi.updateMatch(match);
+            Thread.sleep(2000);
+            SocketClient socketClient = null;
+            try {
+                socketClient = new SocketClient();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            MultiplayerGame game = new MultiplayerGame(5, socketClient);
+            game.setFocusTraversable(true);
+            generalLayout.getChildren().setAll(game);
+            MatchApi.deleteMatch(match);
+        }
+
     }
 
 }
