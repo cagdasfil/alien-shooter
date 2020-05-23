1f7e507b17a2d81b8ec905cc52a9654d3af20f32
cagdas.fil <e2093839@ceng.metu.edu.tr>
2020-05-23 18:43:29 +0300

    Controller implemented for Match model.

---
+++ server/src/main/java/group10/server/api/MatchController.java
@@ -0,0 +1,47 @@
+package group10.server.api;
+
+import group10.server.model.Match;
+import group10.server.model.User;
+import group10.server.service.MatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+public class MatchController {
+
+    private final MatchService matchService;
+
+    @Autowired
+    public MatchController(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
+
+
+    @GetMapping("/matches")
+    public List<Match> getMatches(){
+        return matchService.getAllMatches();
+    }
+
+
+    @PutMapping("/matches/{id}")
+    public void updateMatch(@RequestBody Match match, @PathVariable(value = "id") Long matchId) {
+        matchService.updateMatch(matchId,match);
+    }
+
+    @PostMapping("/add_match")
+    public void createMatch(@Valid @RequestBody Match match) {
+        matchService.addMatch(match);
+    }
+
+
+    @DeleteMapping("/matches/{id}")
+    public void deleteMatch(@PathVariable(value = "id") Long matchId){
+        matchService.deleteMatch(matchId);
+    }
+
+}
