f6bece611b79734ce8acf6b260d63812278a7ed8
cagdas.fil <e2093839@ceng.metu.edu.tr>
2020-06-01 13:04:10 +0300

    waiting screens improved.

--- client/src/main/java/group10/client/controller/WaitingRoomController.java
+++ client/src/main/java/group10/client/controller/WaitingRoomController.java
@@ -8,9 +8,11 @@ import group10.client.model.server.Match;
 import group10.client.multiplayer.MultiplayerGame;
 import group10.client.multiplayer.SocketClient;
 import group10.client.multiplayer.SocketServer;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -35,7 +37,7 @@ public class WaitingRoomController implements Initializable {
 
     @FXML public Pane generalLayout;
     @FXML public Text readyText;
-    @FXML public Button readyButton;
+    @FXML public Button playButton;
 
     Match match;
 
@@ -46,6 +48,69 @@ public class WaitingRoomController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        playButton.setVisible(false);
+        try {
+            match = MatchApi.getMatch();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        if(match == null){ // Server
+            MatchApi.addMatch(
+                    LoginController.user.getUsername()
+            );
+            Thread t = new Thread(() -> {
+                do {
+                    try {
+                        match = MatchApi.getMatch();
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
+                } while (Objects.requireNonNull(match).getClient_player().equals(""));
+                playButton.setOnAction(actionEvent ->  {
+                    playClickServer();
+                });
+                playButton.setVisible(true);
+                readyText.setText("You are matched with " + match.getClient_player());
+            });
+            t.start();
+        }
+        else{ // Client
+            match.setClient_player(LoginController.user.getUsername());
+            MatchApi.updateMatch(match);
+            readyText.setText("You are matched with " + match.getServer_player());
+            Thread t = new Thread(() -> {
+                do {
+                    try {
+                        match = MatchApi.getMatch();
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
+                } while (Objects.requireNonNull(match).getStatus().equals("wait"));
+                playButton.setOnAction(actionEvent ->  {
+                    playClickClient();
+                });
+                playButton.setVisible(true);
+            });
+            t.start();
+            /*
+            Thread t = new Thread(() -> {
+                do {
+                    try {
+                        match = MatchApi.getMatch();
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
+                } while (Objects.requireNonNull(match).getStatus().equals("wait"));
+                playButton.setOnAction(actionEvent ->  {
+                    playClickClient();
+                });
+                playButton.fire();
+            });
+            t.start();
+            //MatchApi.deleteMatch(match);
+
+             */
+        }
 
     }
 
@@ -53,7 +118,7 @@ public class WaitingRoomController implements Initializable {
     public void readyClick() throws IOException, InterruptedException {
 
         match = MatchApi.getMatch();
-        Button playButton = readyButton;
+        Button playButton = new Button();
         Text text = readyText;
         //text.setX(100);
         //text.setY(100);
@@ -90,6 +155,7 @@ public class WaitingRoomController implements Initializable {
     public void playClickServer(){
         match.setStatus("ready");
         MatchApi.updateMatch(match);
+        //readyText.setText("Waiting Teammate's response...");
         MultiplayerGame game = new MultiplayerGame(5, 0);
         game.setFocusTraversable(true);
         generalLayout.getChildren().setAll(game);
